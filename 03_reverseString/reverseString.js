1aa4f4f7aef337d1c93a420c0c285fbdf079ad85
ivooo369 <iveliniliev795@gmail.com>
2022-08-16 23:16:17 +0300

    Solve first three exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let splitString = string.split("");
+  let reversedArray = splitString.reverse();
+  let joinArray = reversedArray.join("");
 
+  return joinArray;
 };
 
 // Do not edit below this line
