c6e814330afec089b87009b5036da0e397dcbbbd
ivooo369 <iveliniliev795@gmail.com>
2022-08-17 23:07:23 +0300

    Add solution for the seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (fah) {
+  fah = (fah - 32) * (5 / 9);
+  fahRounded = Math.round(fah * 10) / 10;
+  return fahRounded;
 };
 
-const ctof = function() {
-
+const ctof = function (cels) {
+  cels = cels * (9 / 5) + 32;
+  celsRounded = Math.round(cels * 10) / 10;
+  return celsRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
