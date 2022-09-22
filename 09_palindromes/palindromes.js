854ccbdcd15b72dc25681916605f20be03554298
ivooo369 <iveliniliev795@gmail.com>
2022-09-22 13:29:01 +0300

    Add solution for palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const re = /[\W_]/g;
+  const lowRegString = string.toLowerCase().replace(re, "");
+  const reverseString = lowRegString.split("").reverse().join("");
+  return reverseString === lowRegString;
 };
 
 // Do not edit below this line
