fd4196a76ac53c23418a1c85babddb3099890140
ivooo369 <iveliniliev795@gmail.com>
2022-09-22 19:38:39 +0300

    Add solution for findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((previousPerson, currentPerson) => {
+    if (isPreviousPersonOlder(previousPerson, currentPerson)) {
+      return previousPerson;
+    } else {
+      return currentPerson;
+    }
+  });
 
+  function isPreviousPersonOlder(previousPerson, currentPerson) {
+    if (previousPerson.yearOfDeath === undefined) {
+      previousPerson.yearOfDeath = new Date().getFullYear();
+    }
+    if (
+      previousPerson.yearOfDeath - previousPerson.yearOfBirth >
+      currentPerson.yearOfDeath - currentPerson.yearOfBirth
+    ) {
+      return true;
+    }
+  }
 };
 
 // Do not edit below this line
